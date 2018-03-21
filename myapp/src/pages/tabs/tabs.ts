317ef1a6cd22376df2690662b3951178d83c5fea
ichengll <937630855@qq.com>
2018-03-21 18:01:35 +0800

    app

---
+++ myapp/src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { OrderPage } from '../order/order';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = ContactPage;
+  tab3Root = OrderPage;
+
+  constructor() {
+
+  }
+}
